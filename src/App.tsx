90402a9e8e0b34161389ef6182c15363524f60e4
EugeneTsalko <tefworkmail@gmail.com>
2022-10-03 22:27:11 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import React, { useContext} from 'react';
-import { CreateProduct } from './components/CreateProduct';
-import { ErrorMessage } from './components/ErrorMessage';
-import { Loader } from './components/Loader';
-import { Modal } from './components/Modal';
-import { Product } from './components/Product';
-import { ModalContext } from './context/ModalContext';
-import { useProducts } from './hooks/products';
-import { IProduct } from './models';
+import { Route, Routes } from 'react-router-dom'
+import { AboutPage } from './pages/AboutPage';
+import { ProductsPage } from './pages/ProductsPage';
 
 function App() {
-  const { loading, error, products, addProduct} = useProducts();
-  const {modal, open, close} = useContext(ModalContext)
-  const createHandler = (product: IProduct) => {
-    close();
-    addProduct(product);
-  }
-
-
   return (
-    <div className='container mx-auto max-width 2xl pt-5'>
-      { loading && <Loader /> }
-      { error && <ErrorMessage error={error} /> }
-      { products.map(product => <Product product={product} key={product.id} />) }
-
-      {modal && <Modal title="Create new product" onClose={close}>
-        <CreateProduct onCreate={createHandler}/>
-      </Modal>}
-
-      <button
-        className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-        onClick={open}
-        >
-          +
-        </button>
-    </div>
+    <Routes>
+      <Route path='/' element={<ProductsPage></ProductsPage>}></Route>
+      <Route path='/about' element={<AboutPage></AboutPage>}></Route>
+    </Routes>
   )
 }
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni minima, illo atque, reiciendis odio dolore velit explicabo temporibus quasi quidem nesciunt eum at, laudantium porro eos quis inventore deleniti incidunt!
+    </div>
+  )
+}
