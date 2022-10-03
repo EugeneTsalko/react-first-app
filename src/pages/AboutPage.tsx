73f808b8eef4da5627f2d6b7c8a65d4832b6431e
EugeneTsalko <tefworkmail@gmail.com>
2022-10-03 22:45:53 +0300

    Updates

--- src/pages/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function AboutPage() {
-  return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni minima, illo atque, reiciendis odio dolore velit explicabo temporibus quasi quidem nesciunt eum at, laudantium porro eos quis inventore deleniti incidunt!
-    </div>
-  )
-}
