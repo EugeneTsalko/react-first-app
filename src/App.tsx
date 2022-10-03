cc0c5ce59e1073a4a241e65f66ea64f56c40c0f9
EugeneTsalko <tefworkmail@gmail.com>
2022-10-03 22:32:37 +0300

    feat: add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
+import { Navigation } from './components/Navigation';
 import { AboutPage } from './pages/AboutPage';
 import { ProductsPage } from './pages/ProductsPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<ProductsPage></ProductsPage>}></Route>
-      <Route path='/about' element={<AboutPage></AboutPage>}></Route>
-    </Routes>
+    <>
+    <Navigation/>
+      <Routes>
+        <Route path='/' element={<ProductsPage></ProductsPage>}></Route>
+        <Route path='/about' element={<AboutPage></AboutPage>}></Route>
+      </Routes>
+    </>
   )
 }
 
