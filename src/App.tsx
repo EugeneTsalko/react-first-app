1c173072b8f47e94be02ad2c37f764bdb3f3f9c0
EugeneTsalko <tefworkmail@gmail.com>
2022-10-03 18:29:54 +0300

    refactor: divide template and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import axios, { AxiosError } from 'axios';
-import { IProduct } from './models';
+import { useProducts } from './hooks/products';
 
 function App() {
+  const { loading, error, products} = useProducts();
 
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-
-  async function fetchProducts() {
-    try {
-      setError('');
-      setLoading(true);
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
-      setProducts(response.data);
-      setLoading(false);
-    } catch (e: unknown) {
-      const error = e as AxiosError;
-      setLoading(false);
-      setError(error.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, [])
 
   return (
     <div className='container mx-auto max-width 2xl pt-5'>
